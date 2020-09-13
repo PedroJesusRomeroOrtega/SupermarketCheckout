a7f8794a1af6c7004ea7b82e724e9b0b4981c2de
Pedro Romero <pedrojesusromeroortega@gmail.com>
2020-09-13 11:15:34 +0200

    feat(angular): update angular version to the latest one (AB#221)

--- src/WebAplication/ClientApp/src/app/app.component.ts
+++ src/WebAplication/ClientApp/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'supermarket-UI';
+  skus: Sku[];
+
+  constructor(
+    private http: HttpClient,
+    @Inject('BASE_URL') private baseUrl: string
+  ) {}
+
+  ngOnInit() {
+    this.http.get<Sku[]>(this.baseUrl + 'api/sku').subscribe(
+      (skus) => {
+        this.skus = skus;
+      },
+      (error) => console.error(error)
+    );
+  }
+}
+
+interface Sku {
+  id: number;
+  name: string;
 }
