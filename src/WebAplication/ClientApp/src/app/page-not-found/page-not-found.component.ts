9ed32dd6901974eb8ad8c89f6153a145ebd087e3
Pedro Romero <pedrojesusromeroortega@gmail.com>
2020-09-16 20:45:41 +0200

    perf: added change detection strategy onPush for all the components

--- src/WebAplication/ClientApp/src/app/page-not-found/page-not-found.component.ts
+++ src/WebAplication/ClientApp/src/app/page-not-found/page-not-found.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
-  styleUrls: ['./page-not-found.component.scss']
+  styleUrls: ['./page-not-found.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PageNotFoundComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
